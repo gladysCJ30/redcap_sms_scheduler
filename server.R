41a55bb938e170a910b3cfdb16c37f3882d82182
Michael Weiland <maw0809@gmail.com>
2018-05-02 16:40:37 -0400

    wip changing the data structure

--- server.R
+++ server.R
@@ -6,20 +6,20 @@ library(dplyr)
 library(RCurl)
 
 values <<- reactiveValues(status = "Send a message", twilio_phone = "7866193595", mydb = NULL)
-values$my_table <<- data.frame(
-  email = as.character(),
-  phone = as.numeric(),
-  quantity = as.numeric(),
-  frequency = as.numeric(),
-  days = as.character(),
-  time_start = I(as.list(as.double())),
-  time_end = I(as.list(as.double())),
-  instrument = as.character(),
-  scheduled_sms = as.double(),
-  sms_body = as.character(),
-  survey_link = as.character(),
-  stringsAsFactors = FALSE
-)
+# values$my_table <<- list(
+#   email = as.character(),
+#   phone = as.numeric(),
+#   quantity = as.numeric(),
+#   frequency = as.numeric(),
+#   days = as.character(),
+#   time_start = I(as.list(as.double())),
+#   time_end = I(as.list(as.double())),
+#   instrument = as.character(),
+#   scheduled_sms = as.double(),
+#   sms_body = as.character(),
+#   survey_link = as.character(),
+#   stringsAsFactors = FALSE
+# )
 values$get_participants <<- 0
 time_range <<- c(strptime("09:00 AM", "%I:%M %p", tz="America/New_York"), strptime("02:00 PM", "%I:%M %p", tz="America/New_York"))
 default_time_range <<- c(strptime("09:00 AM", "%I:%M %p", tz="America/New_York"), strptime("02:00 PM", "%I:%M %p", tz="America/New_York"))
@@ -31,25 +31,39 @@ week_range <<- 0
 autoInvalidate <- reactiveTimer(2000)
 
 observeEvent(autoInvalidate(), {
+  
+  todo_mail <<- names(values$my_table$quantity[values$my_table$quantity > 0])
 
-  todo_mail <- values$my_table[values$my_table$quantity > 0 & values$my_table$scheduled_sms <= Sys.time(),]
-
-  if(nrow(todo_mail) > 0) {
-    for(recipient in row.names(todo_mail)) {
-
-      values$my_table[recipient,]$quantity <<- values$my_table[recipient,]$quantity - 1
-      values$my_table[recipient,] <<- GetNextScheduleTime(values$my_table, recipient)
-
-      tw_send_message(from = values$twilio_phone, to = 3057930763,#values$my_table[recipient,]$phone,
-                      body = paste(as.character(values$my_table[recipient,]$sms_body), as.character(values$my_table[recipient,]$survey_link)))
+  if(length(todo_mail) > 0) {
+    
+    lapply(values$my_table$scheduled_sms, function(scheduled_sms) {
+      
+      print(scheduled_sms)
+      # remove the phone number from the sms-todo list if the scheduled sms delivery time has not arrived yet
+      if(scheduled_sms > Sys.time()) {
+        todo_mail <<- todo_mail[-scheduled_sms]
+      }
+    })
+  
+    todo_mail <- values$my_table[values$my_table$quantity > 0 & values$my_table$scheduled_sms <= Sys.time(),]
+  
+    if(nrow(todo_mail) > 0) {
+      for(recipient in row.names(todo_mail)) {
+        
+        values$my_table[recipient,]$quantity <<- values$my_table[recipient,]$quantity - 1
+        values$my_table[recipient,] <<- GetNextScheduleTime(values$my_table, recipient)
+  
+        tw_send_message(from = values$twilio_phone, to = 3057930763,#values$my_table[recipient,]$phone,
+                        body = paste(as.character(values$my_table[recipient,]$sms_body), as.character(values$my_table[recipient,]$survey_link)))
+      }
     }
-  }
-
-  #30 seconds before next text is due, refresh the survey links in the table
-  new_surveys <- values$my_table[values$my_table$quantity > 0 & values$my_table$scheduled_sms - 30 <= Sys.time(),]
-
-  if(nrow(new_surveys) > 0) {
-    values$get_participants <<- values$get_participants + 1
+  
+    #30 seconds before next text is due, refresh the survey links in the table
+    new_surveys <- values$my_table[values$my_table$quantity > 0 & values$my_table$scheduled_sms - 30 <= Sys.time(),]
+  
+    if(nrow(new_surveys) > 0) {
+      values$get_participants <<- values$get_participants + 1
+    }
   }
 })
 
@@ -89,23 +103,35 @@ observeEvent(values$get_participants, ignoreNULL = F, ignoreInit = F, {
       
       else {
         
-        values$my_table <<- rbind(
-          values$my_table,
-          data.frame(
-            email = tail(instrument_records[instrument_records$record == record_id,]$email, 1),
-            phone = this_phone,
-            quantity = 0,
-            frequency = 30,
-            days = "",
-            time_start = I(list(as.POSIXct(default_time_range[1]))),
-            time_end = I(list(as.POSIXct(default_time_range[2]))),
-            instrument = instrument,
-            scheduled_sms = as.POSIXlt(Sys.time(), tz = "UTC"),
-            sms_body = "",
-            survey_link = survey_link,
-            stringsAsFactors = FALSE
-          )
-        )
+        # values$my_table <<- rbind(
+        #   values$my_table,
+        #   list(
+        #     email = tail(instrument_records[instrument_records$record == record_id,]$email, 1),
+        #     phone = this_phone,
+        #     quantity = 0,
+        #     frequency = 30,
+        #     days = "",
+        #     time_start = I(list(as.POSIXct(default_time_range[1]))),
+        #     time_end = I(list(as.POSIXct(default_time_range[2]))),
+        #     instrument = instrument,
+        #     scheduled_sms = as.POSIXlt(Sys.time(), tz = "UTC"),
+        #     sms_body = "",
+        #     survey_link = survey_link,
+        #     stringsAsFactors = FALSE
+        #   )
+        # )
+        
+        values$my_table$email[[this_phone]] <<- c(values$my_table[[this_phone]]$email, tail(instrument_records[instrument_records$record == record_id,]$email, 1))
+        values$my_table$phone[[this_phone]] <<- this_phone
+        values$my_table$quantity[[this_phone]] <<- 0
+        values$my_table$frequency[[this_phone]] <<- 30
+        values$my_table$days[[this_phone]] <<- ""
+        values$my_table$time_start[[this_phone]]$time_start_1 <<- as.POSIXct(default_time_range[1])
+        values$my_table$time_end[[this_phone]]$time_end_1 <<- as.POSIXct(default_time_range[2])
+        values$my_table$instrument[[this_phone]] <<- instrument
+        values$my_table$scheduled_sms[[this_phone]] <<- as.POSIXlt(Sys.time(), tz = "UTC")
+        values$my_table$sms_body[[this_phone]] <<- ""
+        values$my_table$survey_link[[this_phone]] <<- survey_link
       }
     }
   }
@@ -145,7 +171,7 @@ shinyServer(function(input, output) {
         inputTagList <<- tagAppendChild(inputTagList, sliderInput(
           inputId = paste0("time_range_", i),
           label = paste0("Time Range ", i),
-          value = c(as.POSIXct(time_ranges$time_start[[i]], "%I:%M %p", tz = "America/New_York"), as.POSIXct(time_ranges$time_end[[i]], "%I:%M %p", tz="America/New_York")),
+          value = c(as.POSIXct(time_ranges$time_start[[paste0("time_start_", i)]], "%I:%M %p", tz = "America/New_York"), as.POSIXct(time_ranges$time_end[[paste0("time_end_", i)]], "%I:%M %p", tz="America/New_York")),
           min = strptime("06:00 AM", "%I:%M %p", tz = "America/New_York"),
           max = strptime("09:00 PM", "%I:%M %p", tz = "America/New_York"),
           dragRange = T,
@@ -197,24 +223,28 @@ shinyServer(function(input, output) {
       
       #values$my_table[input$my_table_rows_selected,]$time_start <<- list(values$my_table[input$my_table_rows_selected,]$time_start ,as.POSIXct(default_time_range[1]))
       #values$my_table[input$my_table_rows_selected,]$time_end <<- list(values$my_table[input$my_table_rows_selected,]$time_start ,as.POSIXct(default_time_range[2]))
-      print(7)
-      values$my_table <<- rbind(
-        values$my_table[-input$my_table_rows_selected,],
-        data.frame(
-          email = values$my_table[input$my_table_rows_selected,]$email,
-          phone = values$my_table[input$my_table_rows_selected,]$phone,
-          quantity = values$my_table[input$my_table_rows_selected,]$quantity,
-          frequency = values$my_table[input$my_table_rows_selected,]$frequency,
-          days = values$my_table[input$my_table_rows_selected,]$days,
-          time_start = I(c(values$my_table[input$my_table_rows_selected,]$time_start , I(list(as.POSIXct(default_time_range[1]))))),
-          time_end = I(c(values$my_table[input$my_table_rows_selected,]$time_end , I(list(as.POSIXct(default_time_range[2]))))),
-          instrument = values$my_table[input$my_table_rows_selected,]$instrument,
-          scheduled_sms = values$my_table[input$my_table_rows_selected,]$scheduled_sms,
-          sms_body = values$my_table[input$my_table_rows_selected,]$sms_body,
-          survey_link = values$my_table[input$my_table_rows_selected,]$survey_link,
-          stringsAsFactors = FALSE
-        )
-      )
+
+      # values$my_table <<- rbind(
+      #   values$my_table[-input$my_table_rows_selected,],
+      #   data.frame(
+      #     email = values$my_table[input$my_table_rows_selected,]$email,
+      #     phone = values$my_table[input$my_table_rows_selected,]$phone,
+      #     quantity = values$my_table[input$my_table_rows_selected,]$quantity,
+      #     frequency = values$my_table[input$my_table_rows_selected,]$frequency,
+      #     days = values$my_table[input$my_table_rows_selected,]$days,
+      #     time_start = I(c(values$my_table[input$my_table_rows_selected,]$time_start , I(as.POSIXct(default_time_range[1])))),
+      #     time_end = I(c(values$my_table[input$my_table_rows_selected,]$time_end , I(as.POSIXct(default_time_range[2])))),
+      #     instrument = values$my_table[input$my_table_rows_selected,]$instrument,
+      #     scheduled_sms = values$my_table[input$my_table_rows_selected,]$scheduled_sms,
+      #     sms_body = values$my_table[input$my_table_rows_selected,]$sms_body,
+      #     survey_link = values$my_table[input$my_table_rows_selected,]$survey_link,
+      #     stringsAsFactors = FALSE
+      #   )
+      # )
+      
+      num_time_ranges <- length(values$my_table[input$my_table_rows_selected,]$time_start)
+      values$my_table[input$my_table_rows_selected,]$time_start[paste0("time_start_", num_time_ranges + 1)] <<- as.POSIXct(default_time_range[1])
+      values$my_table[input$my_table_rows_selected,]$time_end[paste0("time_end_", num_time_ranges + 1)] <<- as.POSIXct(default_time_range[2])
     }
   })
   
@@ -232,8 +262,8 @@ shinyServer(function(input, output) {
       
       sapply(1:num_times, function(i) {
         
-        values$my_table[input$my_table_rows_selected,]$time_start[[i]] <<- input[[paste0("time_range_", i)]][1]
-        values$my_table[input$my_table_rows_selected,]$time_end[[i]] <<- input[[paste0("time_range_", i)]][2]
+        values$my_table[input$my_table_rows_selected,]$time_start[[paste0("time_start_", i)]] <<- input[[paste0("time_range_", i)]][1]
+        values$my_table[input$my_table_rows_selected,]$time_end[[paste0("time_end_", i)]] <<- input[[paste0("time_range_", i)]][2]
       })
     }
   })
@@ -244,8 +274,8 @@ shinyServer(function(input, output) {
     
     if(is.null(input$my_table_rows_selected) == F) {
       
-      values$my_table[input$my_table_rows_selected,]$time_start <<- I(list(as.POSIXct(default_time_range[1])))
-      values$my_table[input$my_table_rows_selected,]$time_end <<- I(list(as.POSIXct(default_time_range[2])))
+      values$my_table[input$my_table_rows_selected,]$time_start <<- c(time_start_1 = as.POSIXct(default_time_range[1]))
+      values$my_table[input$my_table_rows_selected,]$time_end <<- c(time_end_1 = as.POSIXct(default_time_range[2]))
     }
   })
   
@@ -282,8 +312,8 @@ shinyServer(function(input, output) {
     if(isTruthy(input$my_table_rows_selected)) {
       values$my_table[input$my_table_rows_selected,]$quantity <<- input$sms_quantity
       values$my_table[input$my_table_rows_selected,]$frequency <<- input$sms_frequency
-      values$my_table[input$my_table_rows_selected,]$time_start <<- list(as.POSIXct(time_range[1]))
-      values$my_table[input$my_table_rows_selected,]$time_end <<- list(as.POSIXct(time_range[2]))
+      #alues$my_table[input$my_table_rows_selected,]$time_start <<- list(as.POSIXct(time_range[1]))
+      #values$my_table[input$my_table_rows_selected,]$time_end <<- list(as.POSIXct(time_range[2]))
       values$my_table[input$my_table_rows_selected,]$days <<- toString(input$weekdays_input)
       values$my_table[input$my_table_rows_selected,] <<- GetNextScheduleTime(values$my_table, rownames(values$my_table[input$my_table_rows_selected,])[1])
       values$my_table[input$my_table_rows_selected,]$sms_body <<- input$sms_body
@@ -295,18 +325,27 @@ shinyServer(function(input, output) {
     reset("sms_frequency")
     reset("weekdays_input")
   })
+  
+  observeEvent({
+    input$my_table_rows_selected
+  }, ignoreInit = T, {
+    
+    #TODO update inputs when a row is selected on the table
+  })
 
   output$my_table <- DT::renderDataTable({
     
     temp_table <- values$my_table
     
     temp_table$time_start <- lapply(temp_table$time_start, function(x) {
-      format.POSIXct(x, "%I:%M %p", tz="America/New_York")
+      lapply(x, format.POSIXct, format="%I:%M %p", tz="America/New_York")
     })
     
     temp_table$time_end <- lapply(temp_table$time_end, function(x) {
-      format.POSIXct(x, "%I:%M %p", tz="America/New_York")
+      lapply(x, format.POSIXct, format="%I:%M %p", tz="America/New_York")
     })
+    
+    return(as.data.frame(temp_table))
     
     if(nrow(temp_table) > 0) {
       datatable(temp_table %>%
